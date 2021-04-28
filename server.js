fd88c6ae6ce71d424cdde2c74cfae68c8d7f1ade
ChaseTheCoder <chasefsheaff@gmail.com>
2021-04-27 22:04:30 -0500

    server set up and .gitignore

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express'); //express is a function
 const app = express(); // app is an object
 
+app.get('/somedata', (reques, response) => {
+  response.send('here is your information');
+})
+
 app.listen(3000, ()=>{
   console.log("I am listening");
 })
