30f63fbde69b267405d8070dd223ef0f0ea316c3
ChaseTheCoder <chasefsheaff@gmail.com>
2021-04-27 22:10:23 -0500

    grades array added

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express'); //express is a function
 const app = express(); // app is an object
 
-app.get('/somedata', (reques, response) => {
-  response.send('here is your information');
+const grade = [0,1,2,3,4,5,6,7,8,9,10,11,12]
+
+app.get('/grades/:index', (request, response) => {
+  response.send(grades[request.params.index]);
 })
 
 app.listen(3000, ()=>{
